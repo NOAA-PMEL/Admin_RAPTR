a199c1fbaa77f0de1dc8cd9e395fabd049473faf
Richard Parks <rparks63@outlook.com>
2018-12-01 14:32:44 -0800

    first functional test specced out

---
+++ raptrcontainer/raptr/functional_tests.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+import unittest
+
+class NewVistiorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_access_user_view(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('RAPTR', self.browser.title)
+#        self.fail('Load of RAPTR Dashboard Failed.')
+
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
