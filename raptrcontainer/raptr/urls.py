d489ec49718ffa6cd1d7124f3fef62dad65d024a
Richard Parks <richard.parks@noaa.gov>
2018-12-17 10:32:20 -0800

    added ability for user to change password

--- raptrcontainer/raptr/urls.py
+++ raptrcontainer/raptr/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
+
 from . import views
 
 app_name = 'raptr'
@@ -13,6 +15,16 @@ urlpatterns = [
         'logout/',
         auth_views.LogoutView.as_view(),
         name='logout'
+    ),
+    path(
+        'password_change/',
+        auth_views.PasswordChangeView.as_view(success_url=reverse_lazy('raptr:password_change_done')),
+        name='password_change'
+    ),
+    path(
+        'password_change/done/',
+        auth_views.PasswordChangeDoneView.as_view(),
+        name='password_change_done'
     ),
     path(
         'report/',
