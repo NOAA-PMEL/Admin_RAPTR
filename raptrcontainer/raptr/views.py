68f96a202f95d9a30a9a80a32e723f719773e78d
Richard Parks <richard.parks@noaa.gov>
2018-12-19 15:18:48 -0800

    added context handlers to detail views

--- raptrcontainer/raptr/views.py
+++ raptrcontainer/raptr/views.py
@@ -17,15 +17,29 @@ class ProjectDetailView(DetailView):
     model = Project
     template_name = 'raptr/project_detail.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(ProjectDetailView, self).get_context_data(**kwargs)
+        context['title'] = 'Project Detail'
+        return context
+
 
 class ContactDetailView(DetailView):
     model = Contact
     template_name = 'raptr/contact_detail.html'
 
+    def get_context_data(self, **kwargs):
+        context = super(ContactDetailView, self).get_context_data(**kwargs)
+        context['title'] = 'Contact Detail'
+        return context
 
 class SponsorDetailView(DetailView):
     model = Sponsor
     template_name = 'raptr/sponsor_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(SponsorDetailView, self).get_context_data(**kwargs)
+        context['title'] = 'Sponsor Detail'
+        return context
 
 
 class FilteredProjectListView(SingleTableMixin, FilterView):
