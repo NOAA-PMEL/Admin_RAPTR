02d47fd96ef4e12e83bf638eb4a57a5f149a45f6
Richard Parks <rparks63@outlook.com>
2019-06-26 19:25:29 -0700

    cleaned up code in crada tables

--- raptrcontainer/crada/tables.py
+++ raptrcontainer/crada/tables.py
@@ -11,30 +11,6 @@ class CradaTable(tables.Table):
         model = Crada
         template_name = 'crada/crada_list.html'
         exclude = (
-            'fund_code',
             'id',
-            'oar_accept_date',
-            'project_expiration_date',
-            'project_notes',
             'slug',
-        )
-
-
-class NewFundsTable(tables.Table):
-    project_id = tables.Column(linkify=True, footer='Total:')
-    budget = tables.Column(footer=lambda table: sum(x.budget for x in table.data), attrs={"td": {"align": "right"}})
-
-    class Meta:
-        model = Fundfy
-        template_name = 'raptr/index.html'
-        exclude = (
-            'fcfy',
-            'fund_code',
-            'id',
-            'oar_accept_date',
-            'project_expiration_date',
-            'project_notes',
-            'sponsor',
-            'budget_currency',
-            'fund_type'
         )
