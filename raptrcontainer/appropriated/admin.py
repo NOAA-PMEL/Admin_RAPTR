8b06bd9494550849cf2d84ee34e3f7cab044178a
Richard Parks <richard.parks@noaa.gov>
2019-11-05 10:31:35 -0800

    added admin view for appropriated funds

--- raptrcontainer/appropriated/admin.py
+++ raptrcontainer/appropriated/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import AppropriatedHistory
 
-# Register your models here.
+
+@admin.register(AppropriatedHistory)
+class AppropriatedHistoryAdmin(admin.ModelAdmin):
+    list_display = [
+        'fiscal_year',
+        'source',
+        'dollars_received'
+    ]
+    list_editable = [
+        'dollars_received',
+    ]
+    ordering = [
+        'fiscal_year',
+    ]
